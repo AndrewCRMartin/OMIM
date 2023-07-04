585734a7f0e9b7f5d5ac3b8ed3c3e15faefad109
AndrewCRMartin <andrew@bioinf.org.uk>
2023-07-04 11:18:44 +0100

    setdate.pl - Removed unused line

--- setdate.pl
+++ setdate.pl
@@ -1,6 +1,5 @@
 #!/acrm/usr/local/bin/perl
 
-my($dfile) = shift(ARGV);
 my($s,$m,$h,$d,$mon,$y,$wd,$yd,$isdst) = localtime(time);
 my($themon) = (Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec)[$mon];
 $y+=1900;
